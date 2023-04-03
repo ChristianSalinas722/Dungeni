36189d75e221236521e8ff7a5560ba9d30ba37ff
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2023-04-02 22:35:47 -0500

    Update Health.cs

--- My project (3)/Assets/Health.cs
+++ My project (3)/Assets/Health.cs
@@ -10,9 +10,33 @@ public class Health : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+     void Update(){
+        if(Input.GetKeyDown(KeyCode.D)){
+            Damage(10);
+        }
+        if(Input.GetKeyDown(KeyCode.H));
+    }
+     public void Damage(int amount){
+     if(amount < 0){
+         throw new System.ArgumentOutOfRangeException("Your attack did nothing mortal");
+        }
+        this.hp -= amount;
+    }
+     public void Heal(int amount){
+     if(amount < 0){
+        throw new System.ArgumentOutOfRangeException("Your Healing failed");
+     }
+        bool isOverMaxHealth = hp + amount > MAX_HEALTH;
+        if(hp + amount > MAX_HEALTH){
+         this.hp = MAX_HEALTH;
+        }
+        else{
+            this.hp += amount;   
+        }
+    }
+     private void Die(){
+        Debug.Log("I am Dead");
+        Destroy(gameObject);
     }
 }
+
