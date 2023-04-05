4bb7fb8246839f756e1151216e69845c1646eef4
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2023-04-05 18:46:57 -0500

    added pause menu

--- My project (3)/Assets/Character/Scripts/MainMenu.cs
+++ My project (3)/Assets/Character/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    public void ChangeScence(string sceneName){
+        PauseMenu.GameIsPaused = false;
         SceneManager.LoadScene(sceneName);
    } 
    public void  Quit(){
---
+++ My project (3)/Assets/Character/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private string Menu;
+    
+   public static bool GameIsPaused = false;
+   public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause(){
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu(){
+        Debug.Log("Loading menu");
+       Time.timeScale = 1f;
+        SceneManager.LoadScene(Menu);
+    }
+    public void QuitGame(){
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
