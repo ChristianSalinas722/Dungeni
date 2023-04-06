f1c6ebfb4f2288290b2b4b1dad617642c9c5274b
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2023-04-05 19:07:16 -0500

    Made A dummy options menu

--- My project (3)/Assets/Character/Scripts/MainMenu.cs
+++ My project (3)/Assets/Character/Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject options;
+    public GameObject Menu;
    public void ChangeScence(string sceneName){
         PauseMenu.GameIsPaused = false;
         SceneManager.LoadScene(sceneName);
@@ -13,4 +15,5 @@ public class MainMenu : MonoBehaviour
     Application.Quit();
     Debug.Log("Game Has Ended");
    }
+
 }
